2161364539b0ce0c2ceae671c6ddca466ed4ce4e
Edgar <edgarcrd@gmail.com>
2025-12-10 19:46:44 -0500

    Completed Final Step

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # rock-paper-scissors
+
+rock-paper-scissors is a simple game that allows the user to enter one of the three options for a total of 5 rounds. The computer will generate a random choice every round and the final score will be printed on the screen.
+
+HAVE FUN!
--- script.js
+++ script.js
@@ -1,50 +1,64 @@
-console.log("Hello Edgar")
+const playGame = () => {
 
-const getComputerChoice = () => {
-  let computerHand = 0;
-  let computerChoice = ""
-  computerHand = Math.floor(Math.random() * 3) + 1;
-  if (computerHand === 1) {
-    computerChoice = "rock";
-  } else if (computerHand === 2) {
-    computerChoice = "paper";
-  } else {
-    computerChoice = "scissors"
+  const getComputerChoice = () => {
+    let computerHand = 0;
+    let computerChoice = ""
+    computerHand = Math.floor(Math.random() * 3) + 1;
+    if (computerHand === 1) {
+      computerChoice = "rock";
+    } else if (computerHand === 2) {
+      computerChoice = "paper";
+    } else {
+      computerChoice = "scissors"
+    }
+    return computerChoice;
   }
-  return computerChoice;
-}
 
-const getHumanChoice = () => {
-  let result = prompt("Enter your choice", "");
-  return result;
-}
-
-//console.log(getHumanChoice())
-//console.log(getComputerChoice());
-
-let humanScore = 0
-let computerScore = 0
-
-const playRound = (humanChoice, computerChoice) => {
-  humanChoice = humanChoice.toLowerCase();
-  let score = "";
-  if (humanChoice === computerChoice) {
-    score = "Tie"
-  } else if ((humanChoice === "scissors" && computerChoice === "paper") ||
-             (humanChoice === "paper" && computerChoice === "rock") ||
-             (humanChoice === "rock" && computerChoice === "scissors")) {
-            score = `you win! ${humanChoice} beats ${computerChoice}`
-  } else {
-    score = `you loose! ${computerChoice} beats ${humanChoice}`;
+  const getHumanChoice = () => {
+    let result = prompt("Rock, Paper or Scissors?", "");
+    return result;
   }
-  return score;
+
+  const playRound = (humanChoice, computerChoice) => {
+    humanChoice = humanChoice.toLowerCase();
+    let score = "";
+    if (humanChoice === computerChoice) {
+      score = "Tie!"
+    } else if ((humanChoice === "scissors" && computerChoice === "paper") ||
+              (humanChoice === "paper" && computerChoice === "rock") ||
+              (humanChoice === "rock" && computerChoice === "scissors")) {
+              score = `you win! ${humanChoice} beats ${computerChoice}`
+    } else {
+      score = `you loose! ${computerChoice} beats ${humanChoice}`;
+    }
+    //console.log(score);
+    alert(score)
+    return score;
+  }
+
+  let humanScore = 0;
+  let computerScore = 0;
+
+  for (let i = 0; i < 5; i++) {
+    let humanSelection = getHumanChoice()
+    let computerSelection = getComputerChoice()
+    roundWinner = playRound(humanSelection, computerSelection)
+    if (roundWinner.indexOf("win") > 0) {
+      humanScore++;
+    } else if (roundWinner.indexOf("loose") > 0) {
+      computerScore++;
+    }
+    console.log(`User: ${humanScore}, Computer: ${computerScore}`)
+    alert(`User: ${humanScore}\nComputer: ${computerScore}`)
+  }
+
+  if (humanScore === computerScore) {
+    alert(`It was a Tie. Good game though!\nUser: ${humanScore}\nComputer: ${computerScore}`)
+  } else if (humanScore > computerScore){
+    alert(`Congrats! You are the winner!\nUser: ${humanScore}\nComputer: ${computerScore}`)
+  } else {
+    alert(`Sorry! Not your game\nUser: ${humanScore}\nComputer: ${computerScore}`)
+  }
 }
 
-const humanSelection = getHumanChoice()
-const computerSelection = getComputerChoice()
-
-console.log(humanSelection)
-console.log(computerSelection)
-
-console.log(playRound(humanSelection, computerSelection))
-
+playGame()
